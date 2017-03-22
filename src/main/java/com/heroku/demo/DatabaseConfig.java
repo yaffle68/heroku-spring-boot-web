d7d0b2a5c439cd74961bbb44ed55a4a1b4d6edb5
Simon Harding <simon.harding@businessdecision.com>
2017-03-22 09:39:27 +0100

    Support local run with H2 DB

--- src/main/java/com/heroku/demo/DatabaseConfig.java
+++ src/main/java/com/heroku/demo/DatabaseConfig.java
@@ -4,14 +4,16 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
 
 @Configuration
+@Profile("postgres")
 public class DatabaseConfig {
     @Bean
     @Primary
-    @ConfigurationProperties(prefix = "spring.datasource")
+    @ConfigurationProperties(prefix = "custom.spring.datasource")
     public DataSource dataSource() {
         return new org.apache.tomcat.jdbc.pool.DataSource();
     }
